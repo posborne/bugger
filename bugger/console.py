6ce793707a7ddf91602ed20ae653158a4976cc52
Paul Osborne <osbpau@gmail.com>
2010-10-01 08:24:58 -0500

    Fixed a few annoying bugs introduced in the last chunk of commits
    * We now send \r\n instead of just \n as we would like things to work
      the same across platforms and this seems the best way to accomplish
      that task.
    
    * Fixed errors related to change to avoid covering up builtins.  I
      actually don't have a problem with covering up `bytes` inside a
      function.  It is the best name for the variable and definitely much
      better than adding a trailing underscore to the keyword argument, yuck!
    
    There should be a followup commit in the not too distant future.

--- bugger/console.py
+++ bugger/console.py
@@ -13,16 +13,17 @@ _stderr = sys.stderr
 class StreamInteractiveConsole(code.InteractiveConsole):
     """Interactive console that works off an input and output stream"""
 
-    def __init__(self, input_stream, output_stream, locals_=None):
+    def __init__(self, input_stream, output_stream, locals=None):
         """Initialize an interactive interpreter talking to the provided streams
 
         Both ``input_stream`` and ``output_stream`` are assumed to be file-like
         objects.
         """
-        code.InteractiveConsole.__init__(self, locals_)
+        code.InteractiveConsole.__init__(self, locals)
         self.input_stream = input_stream
         self.output_stream = output_stream
         self._asyn_more = 0
+        self._byte_buffer = ''
 
     def async_init(self, banner=None, ps1=None, ps2=None):
         """Initialize the interpreter when operating in async mode
@@ -48,35 +49,42 @@ class StreamInteractiveConsole(code.InteractiveConsole):
 
         cprt = 'Type "help", "copyright", "credits" or "license" for more information.'
         if banner is None:
-            self.write("Python %s on %s\n%s\n(%s)\n" %
+            self.write("Python %s on %s\r\n%s\r\n(%s)\r\n" % 
                        (sys.version, sys.platform, cprt,
                         self.__class__.__name__))
         else:
-            self.write("%s\n" % str(banner))
+            self.write("%s\r\n" % str(banner))
 
         self.write(sys.ps1)
 
-    def async_recv(self, bytes_=''):
+    def async_recv(self, bytes=''):
         """Notify this console that there is data to receive"""
-        if not bytes_:
-            bytes_ = self.input_stream.read()
+        if not bytes:
+            bytes = self.input_stream.read()
         encoding = getattr(sys.stdin, 'encoding', None)
-
+        unpushed_bytes = ''.join([self._byte_buffer, bytes])
         # split on both \r\n and \n
-        for line in '\n'.join(bytes_.rstrip().split('\r\n')).split('\n'):
-            if line == '\x04': # EOF
-                raise SystemExit
-            if encoding and not isinstance(line, unicode):
-                line = line.decode(encoding)
-            self._asyn_more = self.push(line)
-
+        if not '\n' in unpushed_bytes:
+            self._byte_buffer += bytes
+        else:
+            for line in ('\n'.join(unpushed_bytes.rstrip().split('\r\n'))).split('\n'):
+                if line == '\x04': # EOF
+                    raise SystemExit
+                if encoding and not isinstance(line, unicode):
+                    line = line.decode(encoding)
+                self._asyn_more = self.push(line)
+            
+            # only write prompt if we are done with all lines and we did in
+            # fact receive a line.  This makes things work out nicer if they
+            # can push multiple lines at once (some clients)
             if self._asyn_more:
                 prompt = sys.ps2
             else:
                 prompt = sys.ps1
+            self._byte_buffer = ''
             self.write(prompt)
-
-        return bytes
+    
+            return bytes
 
     def raw_input(self, prompt=''):
         """Override the default behaviour of raw_input to write to the stream"""
@@ -94,10 +102,10 @@ class TelnetInteractiveConsoleServer(object):
 
     """
 
-    def __init__(self, host='0.0.0.0', port=7070, locals_=None):
+    def __init__(self, host='0.0.0.0', port=7070, locals=None):
         self.host = host
         self.port = port
-        self.locals_ = locals_
+        self.locals = locals
         self.has_exit = False
         self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
@@ -134,12 +142,12 @@ class TelnetInteractiveConsoleServer(object):
                 client, _addr = self.server_sock.accept() # accept the connection
                 client_console = StreamInteractiveConsole(client.makefile('r', 0),
                                                           client.makefile('w', 0),
-                                                          self.locals_)
+                                                          self.locals)
                 client_console.async_init()
                 self.client_sockets[client] = client_console
 
             for client in rl:
-                bytes_ = client.recv(1024)
+                bytes = client.recv(1024)
                 if bytes == '': # client disconnect
                     client.close()
                     del self.client_sockets[client]
@@ -148,7 +156,7 @@ class TelnetInteractiveConsoleServer(object):
                     sys.stdout = client_console.output_stream
                     sys.stderr = client_console.output_stream
                     try:
-                        bytes_ = client_console.async_recv(bytes_)
+                        bytes = client_console.async_recv(bytes)
                     except (SystemExit,):
                         sys.stdout = _stdout
                         sys.stderr = _stderr
@@ -158,5 +166,6 @@ class TelnetInteractiveConsoleServer(object):
                         del self.client_sockets[client]
 
 if __name__ == '__main__':
-    console_server = TelnetInteractiveConsoleServer(host='0.0.0.0', port=7070, locals_=locals())
+    print "Starting python telnet server on port 7070"
+    console_server = TelnetInteractiveConsoleServer(host='0.0.0.0', port=7070, locals=locals())
     console_server.accept_interactions()
